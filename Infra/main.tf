7d2b8e5045ea00de4a3ea71248dd22b2b5cd11ca
deepikathummala <113160547+deepikathummala@users.noreply.github.com>
2026-06-24 11:11:33 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -4,3 +4,10 @@ rg_name  = var.rg_name
 location = var.location
 create_rg_name = true
 }
+module "kv" {
+ source  "./modules/keyvault"
+ keyvault_name = var.keyvault_name
+ location = var.location
+ create_keyvault = true
+ tenant_id = "17563e80-c657-47ec-938e-b1b0ba5df9ec"
+}
