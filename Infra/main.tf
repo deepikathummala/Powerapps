a69f13f9be4df0ef7852a79acd2e0134f525310a
deepikathummala <113160547+deepikathummala@users.noreply.github.com>
2026-06-24 11:55:40 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,13 +2,13 @@ module "resource_group" {
 source = "./modules/resourcegroup"
 rg_name  = var.rg_name 
 location = var.location
-create_rg_name = true
+create_rg_name = var.create_rg_name
 }
 module "kv" {
 source = "./modules/keyvault"
 keyvault_name = var.keyvault_name
 location = var.location
-create_keyvault = true
+create_keyvault = var.create_keyvault
 tenant_id = "17563e80-c657-47ec-938e-b1b0ba5df9ec"
 env = "dev"
 rg_name  = var.rg_name
