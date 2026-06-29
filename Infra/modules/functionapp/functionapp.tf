9d5d3fde0849fab231f66d6468f52db6d123a7a1
deepikathummala <113160547+deepikathummala@users.noreply.github.com>
2026-06-29 18:05:18 +0530

    Update functionapp.tf

--- Infra/modules/functionapp/functionapp.tf
+++ Infra/modules/functionapp/functionapp.tf
@@ -11,7 +11,8 @@ data "azurerm_application_insights" "appins" {
 resource "azurerm_windows_function_app" "functionapp" {
   name = var.func_name
   location = var.location
-  resource_group_name = var.rg_name 
+  resource_group_name = var.rg_name
+  service_plan_id = data.azurerm_service_plan.appplan.id
   identity {
     type = "SystemAssigned"
   }
