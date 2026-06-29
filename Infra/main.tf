afc4cb31ea73508a75611bb0e76debd9f71db3a2
deepikathummala <113160547+deepikathummala@users.noreply.github.com>
2026-06-29 16:41:30 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -13,3 +13,14 @@ tenant_id = "17563e80-c657-47ec-938e-b1b0ba5df9ec"
 env = "dev"
 rg_name  = var.rg_name
 }
+module "functionapp" {
+count = var.create_func ? 1 : 0
+source = "./modules/functionapp"
+rg_name  = var.rg_name 
+location = var.location
+appserviceplan_name = var.appserviceplan_name
+appinsghts_name = var.appinsghts_name
+st_name = var.st_name
+env = var.env
+}
+
