d11c1cd3889821d0eda4b6677bacdb9597bfd2d3
deepikathummala <113160547+deepikathummala@users.noreply.github.com>
2026-06-29 16:35:48 +0530

    Create variables.tf

---
+++ Infra/modules/functionapp/variables.tf
@@ -0,0 +1,20 @@
+variable "func_name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+variable "rg_name" {
+  type = string
+}
+variable "appinsghts_name" {
+  type = string
+  
+}
+variable "appserviceplan_name" {
+  type = string
+}
+variable "st_name" {
+  type = string
+}
